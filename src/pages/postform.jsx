20ca5201b070126124b429dd51b10afb7c78b88d
Aman <jaswal.amansingh@gmail.com>
2021-03-23 12:41:05 +0530

    postform submit

--- src/pages/postform.jsx
+++ src/pages/postform.jsx
@@ -2,15 +2,24 @@ import { Block, Button, List, ListInput, Page } from 'framework7-react'
 import React, { useEffect, useState } from 'react'
 
 export default ()=>{
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+
+    const submit = ()=>{
+        console.log(title, body)
+    }
+
 
     return (
         <Block>
             <h1>Add Posts</h1>
             <List noHairlinesMd inlineLabels>
-                <ListInput name="title" label="Title:" />
+                <ListInput name="title" label="Title:" onChange={e=>setTitle(e.target.value)} />
 
-                <ListInput name="body" label="Body:" />
-                <Button label="Submit" onClick={()=>{console.log('submit')}} />
+                <ListInput name="body" label="Body:" onChange={e=>setBody(e.target.value)} />
+                <br/>
+                <Button fill round onClick={submit} >Submit</Button>
             </List>
         </Block>
     )
